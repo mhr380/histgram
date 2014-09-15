7840b193d4584108575861a1b7d8aa6391568d52
hmiha <hajimemihara1108@gmail.com>
2014-09-15 10:26:36 +0900

    三原色ヒストグラム対応

--- histgram.py
+++ histgram.py
@@ -4,11 +4,11 @@ import cv2
 import numpy as np
 import sys
 
-def imgReader(filename):
-    src = cv2.imread(filename, 0)
+def imgReader(filename, flag):
+    src = cv2.imread(filename, flag)
     return src
 
-def hist(src):
+def hist(src, color):
     # init histgram array
     hist = [0] * 256
 
@@ -23,22 +23,30 @@ def hist(src):
     # get maxlevel to normarize
     maxLevel = max(hist)
 
-    # init histgram img
-    histImg = 255 * np.ones((256, 256))
+    if color == None:
+        # init histgram img
+        histImg = 255 * np.ones((256, 256))
+    else:
+        histImg = 255 * np.ones((256, 256, 3))
+
+    print color 
 
     # draw histgram
     for i in range(256):
         level = hist[i] 
         height = float(maxLevel - level) / maxLevel * 256
-        histImg[height : 256, i] = 0
+
+        if color == None:
+            histImg[height : 256, i] = 0
+        else:
+            histImg[height : 256, i, :] = 0
+            histImg[height : 256, i, color] = 255
 
     # show and save histgram
-    cv2.namedWindow("InputImg")
     cv2.namedWindow("Histgram")
-    cv2.imshow("InputImg", src)
     cv2.imshow("Histgram", histImg)
     cv2.waitKey(0)
-    cv2.imwrite("hist.png", histImg)
+    cv2.imwrite("hist"+str(color)+".png", histImg)
 
     return 
 
@@ -51,11 +59,18 @@ def main():
     filename = argvs[1]
 
     # readImage
-    src = imgReader(filename)
+    src = cv2.imread(filename, 1)
+    cv2.namedWindow("InputImg")
+    cv2.imshow("InputImg", src)
 
-    # get histgram
-    hist(src)
-    
+    # get histgram per color
+    for color in range(3):
+        hist(src[:,:,color], color)
+
+    # get histgram of whole image
+    src = imgReader(filename, 0)
+    hist(src, None)
+
     return
 
 if __name__ == "__main__":
